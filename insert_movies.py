f476f1228fdb67def2037067286ce3d5863cd787
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-11-17 15:51:16 +0200

    pushed the data from movie_list to db

---
+++ insert_movies.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import sessionmaker
+from model import Movies, engine
+from movie_list import movies
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+if __name__ == "__main__":
+    for name, year, rating in movies:
+        session.add(Movies(name, year, rating))
+
+session.commit()
+session.close()
