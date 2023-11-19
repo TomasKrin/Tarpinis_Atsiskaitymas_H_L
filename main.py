787eb9055270448d1b34e64800bcea52bd214c45
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-11-19 13:52:33 +0200

    annotations, implemented highscore adding, minor changes

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import random
+import os
 from sqlalchemy.orm import sessionmaker
 from model import Movies, PlayerScores, engine
 from assets.art import logo, vs
-import os
+from typing import Type
+from playerscores_operations import add_highscore, get_highscores
+from movies_operations import add_all_movies
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
+all_movies: list[Movies] = session.query(Movies).all()
 
-def clear_screen():  # +
+if len(all_movies) == 0:
+    add_all_movies()
+
+
+def clear_screen() -> None:  # +
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def assign_random_movie():  # +
-    all_movies = session.query(Movies).all()
+def assign_random_movie() -> Type[Movies]:  # +
     return session.query(Movies).get(random.randint(1, len(all_movies)))
 
 
-def compare(movie1, movie2, user_input):  # +
-    higher = 0
+def compare(rating1, rating2, user_input) -> bool:  # +
+    higher: int = 0
 
-    if movie1.rating > movie2.rating:
+    if rating1 > rating2:
         higher = 1
-    elif movie1.rating < movie2.rating:
+    elif rating1 < rating2:
         higher = 2
 
     if higher == user_input:
@@ -31,29 +38,28 @@ def compare(movie1, movie2, user_input):  # +
         return False
 
 
-def play_higher_lower():
+def play_higher_lower() -> None:
     # reikia implementuot update ir instert jeigu jungiasi user_nickname - admin
-    # padaryti highscore sistema, kad saugotu top10 scoru: username - score
-    playing_game = True
-    user_nickname = input('Enter your nickname:\n')
+    playing_game: bool = True
+    user_nickname: str = input('Enter your nickname:\n')
 
     while playing_game:
-        score = 0
-        still_guessing = True
+        score: int = 0
+        still_guessing: bool = True
 
         while still_guessing:
             clear_screen()
             print(logo)
 
-            movie1 = assign_random_movie()
-            movie2 = assign_random_movie()
+            movie1: Type[Movies] = assign_random_movie()
+            movie2: Type[Movies] = assign_random_movie()
 
             if score > 0:
-                movie1 = movie2
-                movie2 = assign_random_movie()
+                movie1: Type[Movies] = movie2
+                movie2: Type[Movies] = assign_random_movie()
 
                 if movie1 == movie2:
-                    movie2 = assign_random_movie()
+                    movie2: Type[Movies] = assign_random_movie()
 
             print(f'Movie name: {movie1.name} - Release year: {movie1.release_year}')
             print(vs)
@@ -62,31 +68,42 @@ def play_higher_lower():
             print('-' * 50)
             print(f'Your current score: {score}')
             print('-' * 50)
-            # Padaryti loopa jeigu iveda neteisinga numeriuka, kad leistu ivesti dar karta
-            try:
-                user_guess = int(input('Enter which movie has a higher rating (1/2): '))
-                if compare(movie1, movie2, user_guess):
-                    score += 1
-                else:
-                    still_guessing = False
-            except ValueError:
-                print('Incorrect Option')
-                still_guessing = False
 
-        play_again = input('Want to Play Again? (y/n): ').lower()
+            while True:
+                try:
+                    user_guess = int(input('Enter which movie has a higher rating (1/2): '))
+
+                    if user_guess not in (1, 2):
+                        print('Incorrect Option')
+                        continue
+
+                    if compare(movie1.rating, movie2.rating, user_guess):
+                        score += 1
+                        break
+                    else:
+                        still_guessing: bool = False
+                        break
+
+                except ValueError:
+                    print('Incorrect Option')
+
+        add_highscore(user_nickname, score)
+        play_again: str = input('Want to Play Again? (y/n): ').lower()
 
         if play_again == 'y':
             continue
         elif play_again == 'n':
-            playing_game = False
+            playing_game: bool = False
             clear_screen()
             print("Game Exited Successfully")
         else:
-            playing_game = False
+            playing_game: bool = False
             print("Invalid Input Taken as no.")
 
 
+# meniu
 want_to_play = input("Do you want to play Higher Lower? (y/n)\n").lower()
+
 if want_to_play == 'y':
     clear_screen()
     play_higher_lower()
@@ -96,5 +113,4 @@ elif want_to_play == 'n':
 else:
     print("Invalid Input, Program Exited.")
 
-session.commit()
 session.close()
