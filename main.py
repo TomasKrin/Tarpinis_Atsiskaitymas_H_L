5c641f9666a773e403af6dfbf9a588ea110339d0
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-11-19 16:56:00 +0200

    implemented menu logic
    implemented menu logic where user who enters with a nickname admin gets new menu, where he can edit or add new movies to the Movies table in DB, also to print all available movies.

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from model import Movies, PlayerScores, engine
 from assets.art import logo, vs
 from typing import Type
 from playerscores_operations import add_highscore, get_highscores
-from movies_operations import add_all_movies
+from movies_operations import add_all_movies, get_all_movies, add_movie, edit_movie
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-all_movies: list[Movies] = session.query(Movies).all()
+all_movies: list[Movies] = get_all_movies()
 
 if len(all_movies) == 0:
     add_all_movies()
@@ -38,11 +38,8 @@ def compare(rating1, rating2, user_input) -> bool:  # +
         return False
 
 
-def play_higher_lower() -> None:
-    # reikia implementuot update ir instert jeigu jungiasi user_nickname - admin
+def play_higher_lower(nickname) -> None:
     playing_game: bool = True
-    user_nickname: str = input('Enter your nickname:\n')
-
     while playing_game:
         score: int = 0
         still_guessing: bool = True
@@ -54,12 +51,11 @@ def play_higher_lower() -> None:
             movie1: Type[Movies] = assign_random_movie()
             movie2: Type[Movies] = assign_random_movie()
 
-            if score > 0:
-                movie1: Type[Movies] = movie2
-                movie2: Type[Movies] = assign_random_movie()
-
+            while True:
                 if movie1 == movie2:
                     movie2: Type[Movies] = assign_random_movie()
+                else:
+                    break
 
             print(f'Movie name: {movie1.name} - Release year: {movie1.release_year}')
             print(vs)
@@ -74,7 +70,7 @@ def play_higher_lower() -> None:
                     user_guess = int(input('Enter which movie has a higher rating (1/2): '))
 
                     if user_guess not in (1, 2):
-                        print('Incorrect Option')
+                        print("Invalid Input")
                         continue
 
                     if compare(movie1.rating, movie2.rating, user_guess):
@@ -85,9 +81,9 @@ def play_higher_lower() -> None:
                         break
 
                 except ValueError:
-                    print('Incorrect Option')
+                    print("Invalid Input")
 
-        add_highscore(user_nickname, score)
+        add_highscore(nickname, score)
         play_again: str = input('Want to Play Again? (y/n): ').lower()
 
         if play_again == 'y':
@@ -102,15 +98,135 @@ def play_higher_lower() -> None:
 
 
 # meniu
-want_to_play = input("Do you want to play Higher Lower? (y/n)\n").lower()
+print(logo)
+while True:
+    user_nickname: str = input('Enter your nickname:\n')
+    if user_nickname != '' and ' ' not in user_nickname:
+        break
+    else:
+        print('Nickname cannot be empty or have multiple words')
 
-if want_to_play == 'y':
+while True:
     clear_screen()
-    play_higher_lower()
-elif want_to_play == 'n':
-    print("Program Exit Successful.")
-    exit()
-else:
-    print("Invalid Input, Program Exited.")
+    print(logo)
+    if user_nickname == 'admin':
+        print('\n1. Play the game'
+              '\n2. Highscores'
+              '\n3. Print all movies'
+              '\n4. Edit a movie'
+              '\n5. Add new movie'
+              '\nq - Quit')
+    else:
+        print('\n1. Play the game'
+              '\n2. Highscores'
+              '\nq - Quit')
+
+    selection = input('\n>>> ')
+    if selection == 'q':
+        print("Program Exit Successful.")
+        exit()
+
+    if selection not in ('1', '2', '3', '4', '5') and user_nickname == 'admin':
+        print("Invalid Input")
+        continue
+    elif selection not in ('1', '2') and user_nickname != 'admin':
+        print("Invalid Input")
+        continue
+
+    if selection == '1':
+        clear_screen()
+        play_higher_lower(user_nickname)
+
+    elif selection == '2':
+        clear_screen()
+        print(logo)
+        highscores: list[PlayerScores] = get_highscores()
+        print('\n----------HIGHSCORES----------')
+
+        for count, player_score in enumerate(highscores, 1):
+            print(f'{count}. {player_score.nickname} - {player_score.score}')
+        input('\nENTER --> Back to menu')
+
+    elif selection == '3':
+        clear_screen()
+        print(logo)
+        print('\n----------MOVIES----------')
+
+        for movie in all_movies:
+            print(f'ID: {movie.id} | name: {movie.name} | release year: {movie.release_year} | rating: {movie.rating}')
+        input('\nENTER --> Back to menu')
+
+    elif selection == '4':
+        clear_screen()
+        print(logo)
+        print('\n----------EDIT MOVIE----------')
+
+        while True:
+            try:
+                print("NOTE: You can check the movie ID's by selecting menu option number 3")
+                movie_id: int = int(input('Enter ID of the movie you would like to edit (egz. 1): '))
+                break
+            except ValueError:
+                print('Incorrect number of id, for egz.: 1')
+
+        print('Which part would you like to edit?')
+        print('\n1. Name'
+              '\n2. Release year'
+              '\n3. Rating'
+              '\nb - Back to main menu')
+
+        selected = input('\n>>> ')
+
+        if selected == 'b':
+            continue
+
+        if selected not in ('1', '2', '3'):
+            print('Invalid input')
+            continue
+
+        new_value: str | float = ''
+
+        if selected == '1':
+            new_value: str = input(f'Enter a new movie name value: ')
+
+        elif selected == '2':
+            while True:
+                new_value: str = input(f'Enter a new release year value: ')
+                if (len(new_value) == 4) and (new_value.isdigit()) and (new_value[:2] in ('20', '19', '18')):
+                    break
+                else:
+                    print('Enter a correct year, for egz.: 1972 \n')
+
+        elif selected == '3':
+            while True:
+                try:
+                    new_value: float = float(input('Enter a new rating value (egz: 9.2): '))
+                    break
+                except ValueError:
+                    print('Invalid rating value, should be for egz.: 9.2:')
+
+        if new_value != '':
+            edit_movie(movie_id, selected, new_value)
+        else:
+            print('New value cannot be empty')
+
+        input('\nENTER --> Back to menu')
+
+    elif selection == '5':
+        clear_screen()
+        print(logo)
+        print('\n----------ADD MOVIE----------')
+        name: str = input('Enter a name of the movie (egz. The Godfather): ')
+        year: str = input('Enter release year of the movie (egz. 1972): ')
+        while True:
+            try:
+                rating: float = float(input('Enter movie rating (egz. 9.2): '))
+                break
+            except ValueError:
+                print('Invalid rating value, should be for egz.: 9.2:')
+        add_movie(name, year, rating)
+        input('\nENTER --> Back to menu')
+    else:
+        print("Invalid Input")
 
 session.close()
