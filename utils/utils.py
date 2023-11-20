f2583ef642e3c7f81a92d8662b4e176f296dd045
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-11-20 10:38:01 +0200

    created 3 functions to manage possible bad inputs

--- utils/utils.py
+++ utils/utils.py
@@ -17,3 +17,30 @@ def compare(rating1: float, rating2: float, user_input: int) -> bool:  # +
         return True
     else:
         return False
+
+
+def check_inp_float(msg: str) -> float:
+    while True:
+        try:
+            return float(input(msg))
+        except ValueError:
+            print('Invalid rating value, should be for egz.: 9.2:')
+
+
+def check_inp_empty_str(msg: str) -> str:
+    while True:
+        name: str = input(msg)
+        if name != '':
+            return name
+        else:
+            print('Field, cannot be empty')
+            continue
+
+
+def check_inp_year(msg: str) -> str:
+    while True:
+        year: str = input(msg)
+        if (len(year) == 4) and (year.isdigit()) and (year[:2] in ('20', '19', '18')):
+            return year
+        else:
+            print('Enter a correct year, for egz.: 1972 \n')
