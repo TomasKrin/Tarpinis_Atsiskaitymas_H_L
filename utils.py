e9a46ba9bea632a2c8b707de37cfc8b0f8e89ca1
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-11-20 13:05:07 +0200

    compare function edited
    if rating is equal for movie1 or movie2 it will take either result

--- utils.py
+++ utils.py
@@ -8,9 +8,9 @@ def clear_screen() -> None:  # +
 def compare(rating1: float, rating2: float, user_input: int) -> bool:  # +
     higher: int = 0
 
-    if rating1 > rating2:
+    if rating1 >= rating2:
         higher = 1
-    elif rating1 < rating2:
+    elif rating1 <= rating2:
         higher = 2
 
     if higher == user_input:
